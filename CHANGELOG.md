a193d7d6d581c77305589a1961b6e8b750033198
Jeb Rosen <jeb@jebrosen.com>
2021-06-26 17:59:18 -0700

    docs: update `query` module documentation and link to the latest applicable standards
    And document the new module in the CHANGELOG.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,11 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## Unreleased
+### Added
+- Added the `query` module, including constants for some commonly used
+  header and parameter values for use by `Adapter` implementations.
+
 ## 0.5.0-rc.1 - 2021-06-09
 ### Changed
 - Updated the `rocket` dependency to `0.5.0-rc.1`
--- src/query.rs
+++ src/query.rs
@@ -1,25 +1,26 @@
-//! Constants corresponding to standard HTTP names.
+//! Constants for commonly used header and parameter names and values.
 
-/// Common constants for HTTP headers
+/// Constants for commonly used HTTP header values.
 pub mod header {
-    /// `application/json` value of [`Accept`] header corresponding to [JSON]-formatted data.
+    /// `application/json` value of the [`Accept`] header, corresponding to
+    /// [JSON]-formatted data.
     ///
     /// [`Accept`]: https://datatracker.ietf.org/doc/html/rfc7231#section-5.3.2
-    /// [JSON]: https://datatracker.ietf.org/doc/html/rfc7159
+    /// [JSON]: https://datatracker.ietf.org/doc/html/rfc8259
     pub const APPLICATION_JSON: &str = "application/json";
 
-    /// `application/x-www-form-urlencoded` value of [`Content-Type`] header
-    /// corresponding to [form-based] data encoded into a URL.
+    /// `application/x-www-form-urlencoded` value of the [`Content-Type`] header,
+    /// corresponding to [urlencoded] data.
     ///
     /// [`Content-Type`]: https://datatracker.ietf.org/doc/html/rfc7231#section-3.1.1.5
-    /// [Form-based]: https://www.ietf.org/rfc/rfc1867
+    /// [urlencoded]: https://url.spec.whatwg.org/#application/x-www-form-urlencoded
     pub const X_WWW_FORM_URLENCODED: &str = "application/x-www-form-urlencoded";
 }
 
-/// Constants corresponding to URL parameter names
-/// and their common values for OAuth2 implementation.
+/// Constants for commonly used URL parameter names and values.
 pub mod param {
-    /// `client_id`, the client identifier issued to the client during the registration process.
+    /// `client_id`, the client identifier issued to the client during the
+    /// registration process.
     pub const CLIENT_ID: &str = "client_id";
 
     /// `client_secret`, the client secret.
@@ -28,35 +29,38 @@ pub mod param {
     /// `code`, the authorization code received from the authorization server.
     pub const CODE: &str = "code";
 
-    /// `grant_type`, extension grant type.
+    /// `grant_type`, the type of grant being requested from the server.
     pub const GRANT_TYPE: &str = "grant_type";
 
-    /// Common values of `grant_type` parameter.
+    /// Common values of the `grant_type` parameter.
     pub mod grant_type {
-        /// `authorization_code`, grant type for OAuth2 [Access Token Request].
+        /// `authorization_code`, the grant type for an OAuth2 [Access Token Request].
         ///
         /// [Access Token Request]: https://datatracker.ietf.org/doc/html/rfc6749#section-4.1.3
         pub const AUTHORIZATION_CODE: &str = "authorization_code";
 
-        /// `refresh_token`, grant type for OAuth2 [Refreshing an Access Token].
+        /// `refresh_token`, the grant type for [Refreshing an Access Token].
         ///
         /// [Refreshing an Access Token]: https://datatracker.ietf.org/doc/html/rfc6749#section-6
         pub const REFRESH_TOKEN: &str = "refresh_token";
     }
 
-    /// `redirect_uri`, the endpoint to which the authorization server redirects the user-agent.
+    /// `redirect_uri`, the endpoint to which the authorization server will
+    /// redirect the user agent after authorization.
     pub const REDIRECT_URI: &str = "redirect_uri";
 
     /// `refresh_token`, the refresh token, which can be used to obtain
     /// new access tokens using the same authorization grant.
     pub const REFRESH_TOKEN: &str = "refresh_token";
 
-    /// `response_type`, type of response requested from the authorization server.
+    /// `response_type`, the type of response requested from the authorization
+    /// server.
     pub const RESPONSE_TYPE: &str = "response_type";
 
-    /// Common values of `response_type` parameter.
+    /// Common values of the `response_type` parameter.
     pub mod response_type {
-        /// `code`, response type for OAuth2 [Authorization Request].
+        /// `code`, the response type for an OAuth2 [Authorization Request].
+        ///
         /// [Authorization Request]: https://datatracker.ietf.org/doc/html/rfc6749#section-4.1.1
         pub const CODE: &str = "code";
     }
@@ -64,7 +68,7 @@ pub mod param {
     /// `scope`, the scope of the access request.
     pub const SCOPE: &str = "scope";
 
-    /// `state`, an opaque value used by the client
-    /// to maintain state between the request and callback.
+    /// `state`, an opaque value used by the client to maintain state between the
+    /// request and callback.
     pub const STATE: &str = "state";
 }
